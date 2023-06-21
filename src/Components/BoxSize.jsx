7eb28961f306afb0a1a8a1df427807010ad91fb8
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2023-06-21 21:39:55 +0300

    page with sizes

--- src/Components/BoxSize.jsx
+++ src/Components/BoxSize.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import '../Styles/BoxSize/boxSize.css'
 
-const BoxSize = () => {
-  return <div>BoxSize</div>
+const BoxSize = ({ size, sizeNumbers }) => {
+  return (
+    <div className="boxSize__container">
+      <div className={`boxSize__container_${size}`}></div>
+      <div className="boxSize__container_sizes">
+        <p className="boxSize__container_size">{size}</p>
+        <p className="boxSize__container_sizeNumbers">{sizeNumbers}</p>
+      </div>
+    </div>
+  )
 }
 
 export default BoxSize
