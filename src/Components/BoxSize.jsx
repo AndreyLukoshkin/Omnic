9ccc00f3099a93f4487897f58e5bb076b9788184
Andrey Lukoshkin <lukoshkin.andrey@gmail.com>
2023-06-22 10:56:22 +0300

    add functional, change styles

--- src/Components/BoxSize.jsx
+++ src/Components/BoxSize.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../Styles/BoxSize/boxSize.css'
 
-const BoxSize = ({ size, sizeNumbers }) => {
+const BoxSize = ({ size, sizeNumbers, charSize }) => {
   return (
     <div className="boxSize__container">
       <div className={`boxSize__container_${size}`}></div>
       <div className="boxSize__container_sizes">
-        <p className="boxSize__container_size">XS</p>
+        <p className="boxSize__container_size">{charSize}</p>
         <p className="boxSize__container_sizeNumbers">{sizeNumbers} см</p>
       </div>
     </div>
